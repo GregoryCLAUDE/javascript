48b82b37753c30fce8d6f4f6f8a588e43e250c81
Grégory <claudejurado@orange.fr>
2017-03-09 11:49:37 +0100

    amelioration de l'implementation, et step 7

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// step 4
 console.log ("salut tout le monde les gens");
 
 function addition (a,b){
@@ -6,14 +7,28 @@ function addition (a,b){
 
 addition(2,4);
 
+//step 5
+
 function substraction (a,b){
 	console.log (a-b)
 };
 
 substraction (50,45);
 
+//step 6
+
 function multiplication (a,b){
 	console.log(a*b)
 };
 
 multiplication (120, 45)
+
+// step 7
+
+function division (a,b){
+	if (b > 0) console.log(a/b);
+
+	else console.log("Can't divide by 0") };
+
+division (25, 5)
+division (25, 0)
